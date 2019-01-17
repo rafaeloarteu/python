d833cb429c2fc303fc898249c2beb80f84e2d9bf
rafaeloart <rafaeloartpl@gmail.com>
2019-01-17 12:19:41 +0100

    Add files via upload

---
+++ 1-1-liczbyodstepy.py
@@ -0,0 +1,13 @@
+""" Języki skryptowe - Cwiczenia - Rafał Stepkowski GD30982 -
+Napisz program, który liczy za użytkownika. Umożliwi użytkownikowi
+wprowadzenie liczby początkowej, liczby końcowej i wielkości odstępu między kolejnymi
+liczbami. """
+
+start = int(input("Podaj liczbe poczatkowa: "))
+finish = int(input("Podaj liczbe koncowa: "))
+jump = int(input("Podaj wielkosc odstepu miedzy kolejnymi liczbami: "))
+ 
+for digit in range(start, finish, jump):
+   print(digit, end=" ")
+ 
+input("\n\nAby zakonczyc program, nacisnij Enter.")
---
+++ 1-2-odwrotnie.py
@@ -0,0 +1,11 @@
+""" Języki skryptowe - Cwiczenia - Rafał Stepkowski GD30982 -
+
+2. Utwórz program, który wczytuje komunikat użytkownika, a następnie wypisuje go w
+odwrotnej kolejności. """
+
+
+x = input("Podaj wyraz: ")
+print (x[::-1])
+
+
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
